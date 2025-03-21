ae6887d8ebff5f263c26e8b89b803ce244bbe9f6
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2025-03-21 10:28:00 +0300

    Completed Basic router,setup pages and nested pages

---
+++ src/pages/Cocktail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cocktail = () => {
+  return <h2>Coctail</h2>;
+};
+
+export default Cocktail;
---
+++ src/pages/Newsletter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Newsletter = () => {
+  return <h2>newsletter</h2>;
+};
+
+export default Newsletter;
